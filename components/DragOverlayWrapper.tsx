3e59f676a642e1fcc24793a54dc09f42b65a2295
codenamemoe <newmoeali@gmail.com>
2024-03-08 15:14:21 +0000

    designer context

--- components/DragOverlayWrapper.tsx
+++ components/DragOverlayWrapper.tsx
@@ -40,7 +40,11 @@ function DragOverlayWrapper() {
 			const DesignerElementComponent =
 				FormElements[element.type].designerComponent;
 
-			node = <DesignerElementComponent elementInstance={element} />;
+			node = (
+				<div className="flex bg-accent border rounded-md h-[120px] w-full py-2 px-4 opacity-60 pointer-events-none">
+					<DesignerElementComponent elementInstance={element} />;
+				</div>
+			);
 		}
 	}
 
