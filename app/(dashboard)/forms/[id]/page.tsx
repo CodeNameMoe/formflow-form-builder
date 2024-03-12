b5ef66877bef45a24d35b01666fcf003c709c5c4
codenamemoe <newmoeali@gmail.com>
2024-03-12 22:41:18 +0000

    form submission table

--- app/(dashboard)/forms/[id]/page.tsx
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -1,13 +1,16 @@
-import { GetFormById } from "@/actions/form";
+import { GetFormById, GetFormWithSubmissions } from "@/actions/form";
 import FormBuilder from "@/components/FormBuilder";
 import FormLinkShare from "@/components/FormLinkShare";
 import VisitBtn from "@/components/VisitBtn";
-import React from "react";
+import React, { ReactNode } from "react";
 import { StatsCard } from "../../page";
 import { LuView } from "react-icons/lu";
 import { FaWpforms } from "react-icons/fa";
 import { HiCursorClick } from "react-icons/hi";
 import { TbArrowBounce } from "react-icons/tb";
+import { ElementsType, FormElementInstance } from "@/components/FormElements";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { formatDistance } from "date-fns";
 
 async function FormDetailPage({
   params,
@@ -89,10 +92,103 @@ async function FormDetailPage({
 
 export default FormDetailPage;
 
-function SubmissionsTable({ id }: { id: number }) {
-  return (
-    <>
-      <h1>Submissions</h1>
-    </>
-  );
-}
+type Row = { [key: string]: string } & {
+	submittedAt: Date;
+  };
+
+async function SubmissionsTable({ id }: { id: number }) {
+	const form = await GetFormWithSubmissions(id);
+
+	if (!form) {
+		throw new Error("form not found");
+	  }
+
+	  const formElements = JSON.parse(form.content) as FormElementInstance[];
+	  const columns: {
+		id: string;
+		label: string;
+		required: boolean;
+		type: ElementsType;
+	  }[] = [];
+
+	  formElements.forEach((element) => {
+		switch (element.type) {
+		  case "TextField":
+		//   case "NumberField":
+		//   case "TextAreaField":
+		//   case "DateField":
+		//   case "SelectField":
+		//   case "CheckboxField":
+			columns.push({
+			  id: element.id,
+			  label: element.extraAttributes?.label,
+			  required: element.extraAttributes?.required,
+			  type: element.type,
+			});
+			break;
+		  default:
+			break;
+		}
+	  });
+
+	  const rows: Row[] = [];
+	  form.FormSubmissions.forEach((submission) => {
+		const content = JSON.parse(submission.content);
+		rows.push({
+		  ...content,
+		  submittedAt: submission.createdAt,
+		});
+	  });
+	  
+	  return (
+		<>
+		  <h1 className="text-2xl font-bold my-4">Submissions</h1>
+		  <div className="rounded-md border">
+			<Table>
+			  <TableHeader>
+				<TableRow>
+				  {columns.map((column) => (
+					<TableHead key={column.id} className="uppercase">
+					  {column.label}
+					</TableHead>
+				  ))}
+				  <TableHead className="text-muted-foreground text-right uppercase">Submitted at</TableHead>
+				</TableRow>
+			  </TableHeader>
+			  <TableBody>
+				{rows.map((row, index) => (
+				  <TableRow key={index}>
+					{columns.map((column) => (
+					  <RowCell key={column.id} type={column.type} value={row[column.id]} />
+					))}
+					<TableCell className="text-muted-foreground text-right">
+					  {formatDistance(row.submittedAt, new Date(), {
+						addSuffix: true,
+					  })}
+					</TableCell>
+				  </TableRow>
+				))}
+			  </TableBody>
+			</Table>
+		  </div>
+		</>
+	  );
+	}
+
+	function RowCell({ type, value }: { type: ElementsType; value: string }) {
+		let node: ReactNode = value;
+	  
+		// switch (type) {
+		//   case "DateField":
+		// 	if (!value) break;
+		// 	const date = new Date(value);
+		// 	node = <Badge variant={"outline"}>{format(date, "dd/MM/yyyy")}</Badge>;
+		// 	break;
+		//   case "CheckboxField":
+		// 	const checked = value === "true";
+		// 	node = <Checkbox checked={checked} disabled />;
+		// 	break;
+		// }
+	  
+		return <TableCell>{node}</TableCell>;
+	  }
