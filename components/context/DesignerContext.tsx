f71e6e895292c3e6217f9281903067d22e09f25f
codenamemoe <newmoeali@gmail.com>
2024-03-12 11:47:08 +0000

    added 3 drag and drop scenarios

--- components/context/DesignerContext.tsx
+++ components/context/DesignerContext.tsx
@@ -16,6 +16,8 @@ type DesignerContextType = {
 
 	selectedElement: FormElementInstance | null;
 	setSelectedElement: Dispatch<SetStateAction<FormElementInstance | null>>;
+
+	updateElement: (id: string, element: FormElementInstance) => void;
 };
 
 export const DesignerContext = createContext<DesignerContextType | null>(null);
@@ -25,7 +27,7 @@ export default function DesignerContextProvider({
 }: { children: ReactNode }) {
 	const [elements, setElements] = useState<FormElementInstance[]>([]);
 	const [selectedElement, setSelectedElement] =
-		useState<FormElementInstance | null>([]);
+		useState<FormElementInstance | null>(null);
 
 	const addElement = (index: number, element: FormElementInstance) => {
 		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
@@ -40,6 +42,15 @@ export default function DesignerContextProvider({
 		setElements((prev) => prev.filter((element) => element.id !== id));
 	};
 
+	const updateElement = (id: string, element: FormElementInstance) => {
+		setElements((prev) => {
+		  const newElements = [...prev];
+		  const index = newElements.findIndex((el) => el.id === id);
+		  newElements[index] = element;
+		  return newElements;
+		});
+	  };
+
 	return (
 		<DesignerContext.Provider
 			value={{
@@ -49,6 +60,8 @@ export default function DesignerContextProvider({
 
 				selectedElement,
 				setSelectedElement,
+
+				updateElement,
 			}}
 		>
 			{children}
