526bab4771780506c740aad4d2392b30daa13858
codenamemoe <newmoeali@gmail.com>
2024-03-13 11:38:25 +0000

    improved submission table headers

--- app/(dashboard)/forms/[id]/page.tsx
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -10,7 +10,9 @@ import { HiCursorClick } from "react-icons/hi";
 import { TbArrowBounce } from "react-icons/tb";
 import { ElementsType, FormElementInstance } from "@/components/FormElements";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { formatDistance } from "date-fns";
+import { format, formatDistance } from "date-fns";
+import { Badge } from "@/components/ui/badge";
+import { Checkbox } from "@/components/ui/checkbox";
 
 async function FormDetailPage({
   params,
@@ -114,11 +116,11 @@ async function SubmissionsTable({ id }: { id: number }) {
 	  formElements.forEach((element) => {
 		switch (element.type) {
 		  case "TextField":
-		//   case "NumberField":
-		//   case "TextAreaField":
-		//   case "DateField":
-		//   case "SelectField":
-		//   case "CheckboxField":
+		  case "NumberField":
+		  case "TextAreaField":
+		  case "DateField":
+		  case "SelectField":
+		  case "CheckboxField":
 			columns.push({
 			  id: element.id,
 			  label: element.extraAttributes?.label,
@@ -178,17 +180,17 @@ async function SubmissionsTable({ id }: { id: number }) {
 	function RowCell({ type, value }: { type: ElementsType; value: string }) {
 		let node: ReactNode = value;
 	  
-		// switch (type) {
-		//   case "DateField":
-		// 	if (!value) break;
-		// 	const date = new Date(value);
-		// 	node = <Badge variant={"outline"}>{format(date, "dd/MM/yyyy")}</Badge>;
-		// 	break;
-		//   case "CheckboxField":
-		// 	const checked = value === "true";
-		// 	node = <Checkbox checked={checked} disabled />;
-		// 	break;
-		// }
+		switch (type) {
+		  case "DateField":
+			if (!value) break;
+			const date = new Date(value);
+			node = <Badge variant={"outline"}>{format(date, "dd/MM/yyyy")}</Badge>;
+			break;
+		  case "CheckboxField":
+			const checked = value === "true";
+			node = <Checkbox checked={checked} disabled />;
+			break;
+		}
 	  
 		return <TableCell>{node}</TableCell>;
 	  }
